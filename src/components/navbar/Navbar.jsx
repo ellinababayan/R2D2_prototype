a71b97ab93b988182b350aba51400a18bfe78924
Ellina Babaian <famogl375@gmail.com>
2023-09-26 18:22:14 -0700

    setting up navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = () => {};
+const Navbar = () => {
+  return (
+    <div className="navbar__body">
+      <img src="img/logo.webp" alt="Logo img" className="navbar__logo-img" />
+
+      <div className="navbar__navigation">
+        <a href="" className=" navbar__item navbar_signin">
+          Sign In
+        </a>
+        <a href="" className="navbar__item navbar_join">
+          Join The Waitlist
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
