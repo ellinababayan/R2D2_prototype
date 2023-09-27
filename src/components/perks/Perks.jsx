ab430e8a07cd9ac079a506e2d9dc732a94a4a7b9
Ellina Babaian <famogl375@gmail.com>
2023-09-27 16:55:11 -0700

    setting up perks component

--- src/components/perks/Perks.jsx
+++ src/components/perks/Perks.jsx
@@ -1,6 +1,88 @@
 import React from "react";
 import "./Perks.css";
 
-const Perks = () => {};
+const Perks = () => {
+  return (
+    <div className="perk__body">
+      <div className="perk__container">
+        <h1 className="perk-h1">Revolutionize Revenue Research with AutoGPT</h1>
+        <h3 className="perk-h3">
+          Dive deep, equipped with real-time insights, and ace every revenue
+          move.
+        </h3>
+        <div className="perk-table">
+          <div className="perk__item perk__item-1">
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Find Account Intelligence</h2>
+            <p className="perk-description">
+              Use this to find Accounts that match your Ideal Customer Profile
+            </p>
+          </div>
+          <div className="perk__item perk__item-2">
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Find New Leads</h2>
+            <p className="perk-description">
+              Use this to find leads based on your customer persona.
+            </p>
+          </div>
+          <div className="perk__item perk__item-3">
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Person Research</h2>
+            <p className="perk-description">
+              Find everything there is to know about about a contact.
+            </p>
+          </div>
+          <div className="perk__item perk__item-4">
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Company Research</h2>
+            <p className="perk-description">
+              Research everything about a company to get up to speed quickly.
+            </p>
+          </div>
+          <div className="perk__item perk__item-5">
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Find Industry Trends</h2>
+            <p className="perk-description">
+              Research about an industry to get up to spend in minutes.
+            </p>
+          </div>
+          <div className="perk__item perk__item-6">
+            <p className="perks-coming">Coming soon</p>
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Enrich Your Lead List</h2>
+            <p className="perk-description">
+              Upload your lead list to R2D2 to find additional information about
+              them.
+            </p>
+          </div>
+          <div className="perk__item perk__item-7">
+            <p className="perks-coming">Coming soon</p>
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Personalize Sales Outreach</h2>
+            <p className="perk-description">
+              Write personalized sales content like emails, linkedin out reach.
+            </p>
+          </div>
+          <div className="perk__item perk__item-8">
+            <p className="perks-coming">Coming soon</p>
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Find Competitive Info</h2>
+            <p className="perk-description">
+              Find competitors and differentiation for any company.
+            </p>
+          </div>
+          <div className="perk__item perk__item-9">
+            <p className="perks-coming">Coming soon</p>
+            <img src="" alt="" className="perk-img" />
+            <h2 className="perk-h2">Visualize Content</h2>
+            <p className="perk-description">
+              Add content for R2D2 to create into charts and graphs for you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Perks;
