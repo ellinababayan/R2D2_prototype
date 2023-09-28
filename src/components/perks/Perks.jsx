6942fea71d3f733c4f33429dd85709e67835626c
Ellina Babaian <famogl375@gmail.com>
2023-09-27 17:07:29 -0700

    adding images

--- src/components/perks/Perks.jsx
+++ src/components/perks/Perks.jsx
@@ -12,35 +12,35 @@ const Perks = () => {
         </h3>
         <div className="perk-table">
           <div className="perk__item perk__item-1">
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-1.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Find Account Intelligence</h2>
             <p className="perk-description">
               Use this to find Accounts that match your Ideal Customer Profile
             </p>
           </div>
           <div className="perk__item perk__item-2">
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-2.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Find New Leads</h2>
             <p className="perk-description">
               Use this to find leads based on your customer persona.
             </p>
           </div>
           <div className="perk__item perk__item-3">
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-3.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Person Research</h2>
             <p className="perk-description">
               Find everything there is to know about about a contact.
             </p>
           </div>
           <div className="perk__item perk__item-4">
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-4.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Company Research</h2>
             <p className="perk-description">
               Research everything about a company to get up to speed quickly.
             </p>
           </div>
           <div className="perk__item perk__item-5">
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-5.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Find Industry Trends</h2>
             <p className="perk-description">
               Research about an industry to get up to spend in minutes.
@@ -48,7 +48,7 @@ const Perks = () => {
           </div>
           <div className="perk__item perk__item-6">
             <p className="perks-coming">Coming soon</p>
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-6.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Enrich Your Lead List</h2>
             <p className="perk-description">
               Upload your lead list to R2D2 to find additional information about
@@ -57,7 +57,7 @@ const Perks = () => {
           </div>
           <div className="perk__item perk__item-7">
             <p className="perks-coming">Coming soon</p>
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-7.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Personalize Sales Outreach</h2>
             <p className="perk-description">
               Write personalized sales content like emails, linkedin out reach.
@@ -65,7 +65,7 @@ const Perks = () => {
           </div>
           <div className="perk__item perk__item-8">
             <p className="perks-coming">Coming soon</p>
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-8.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Find Competitive Info</h2>
             <p className="perk-description">
               Find competitors and differentiation for any company.
@@ -73,7 +73,7 @@ const Perks = () => {
           </div>
           <div className="perk__item perk__item-9">
             <p className="perks-coming">Coming soon</p>
-            <img src="" alt="" className="perk-img" />
+            <img src="img/perk-9.webp" alt="" className="perk-img" />
             <h2 className="perk-h2">Visualize Content</h2>
             <p className="perk-description">
               Add content for R2D2 to create into charts and graphs for you.
