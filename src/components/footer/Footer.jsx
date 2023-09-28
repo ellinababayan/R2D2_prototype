84370c610d4200ae16b141e77d003ff232b37c55
Ellina Babaian <famogl375@gmail.com>
2023-09-27 19:37:08 -0700

    setting up footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import "./Footer.css";
 
-const Footer = () => {}
+const Footer = () => {
+  return (
+    <div className="footer__body">
+      <div className="footer__container">
+        <p className="footer_rights">
+          &copy; 2023 Involve.ai. All rights reserved.
+        </p>
+        <div className="nav">
+          <a href="" className="footer_terms">
+            Terms
+          </a>
+          <a href="" className="footer_policy">
+            Privacy Policy
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
